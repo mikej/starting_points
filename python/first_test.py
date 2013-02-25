549eb0f117a5254b2b5e6cf3b39886c0246990de
Michael Josephson <mike@josephson.org>
2013-02-25 21:33:17 +0000

    Initial commit of Python starting point

---
+++ python/first_test.py
@@ -0,0 +1,11 @@
+import unittest
+
+# import the code to be tested here
+
+class FirstTest(unittest.TestCase):
+    # replace with your tests
+    def test_truth(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
