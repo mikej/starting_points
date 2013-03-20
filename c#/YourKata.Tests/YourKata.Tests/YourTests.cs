675e12b9e9ac4aa92cc95446c8b39d9e37bd19c0
John Nolan <john.nolan@moneyplusgroup.co.uk>
2013-03-20 17:12:50 +0000

    Added nunit project (.net)

---
+++ c#/YourKata.Tests/YourKata.Tests/YourTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace YourKata.Tests
+{
+    [TestFixture]
+    public class YourTests
+    {
+        [Test]
+        public void YourFirstTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
